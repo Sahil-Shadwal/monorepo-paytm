9f0b9858386c0869c586c9e7456b412010fdd6de
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-02 15:11:54 +0530

    prisma done

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,6 @@
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
 export default function Home() {
   return (
     <div className="flex items-center justify-center text-xl">
