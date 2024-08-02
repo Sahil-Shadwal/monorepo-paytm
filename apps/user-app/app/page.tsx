23744a1c1bb1e13a0b1604d43f10ade3dc26ed11
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-08-02 14:54:39 +0530

    setup prisma

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center text-xl">
-      <h1>Hii there</h1>
+      <h1>Hii there jackie</h1>
     </div>
   );
 }
